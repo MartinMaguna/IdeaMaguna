f7d479890bef201c19963acc8982635d8c9483bb
Martín Maguna <martinmaguna33@gmail.com>
2023-11-16 01:21:07 -0300

    Ajustes casi finales

--- JS/main.js
+++ JS/main.js
@@ -11,75 +11,9 @@ const usuarios = [
         username: "usuario3",
         password: "contraseña3"
     },
-];
-
-const peliculas = [
     {
-        titulo: "30 años de oscuridad",
-        director: "Manuel H. Martin",
-        año: 2012,
-        genero: "Documental",
-        duración: "Largometraje",
-        imagen: "assets/img/29.webp",
-    },
-    {
-        titulo: "Años de calle",
-        director: "Alejandra Grinschpun",
-        año: 2013,
-        genero: "Documental",
-        duración: "Largometraje",
-        imagen: "assets/img/34.webp",
-    },
-    {
-        titulo: "Bolishopping",
-        director: "Pablo Stigliani",
-        año: 2014,
-        genero: "Ficción",
-        duración: "Largometraje",
-        imagen: "assets/img/33.webp",
-    },
-    {
-        titulo: "Guaraní",
-        director: "Luis Zorraquín",
-        año: 2016,
-        genero: "Ficción",
-        duración: "Largometraje",
-        imagen: "assets/img/32.webp",
-    },
-    {
-        titulo: "El otro lado de todo",
-        director: "Mira Turajlik",
-        año: 2017,
-        genero: "Ficción",
-        duración: "Largometraje",
-        imagen: "assets/img/30.webp",
-    },
-    {
-        titulo: "A new home",
-        director: "Žiga Virc",
-        año: 2016,
-        genero: "Ficción",
-        duración: "Cortometraje",
-        imagen: "assets/img/28.webp",
-    },
-    {
-        titulo: "Chike",
-        sinopsis: "La adolescencia es muchas veces un camino sinuoso y solitario. Ana y Carla se encuentran para experimentar con sus deseos, determinaciones y miedos para vivir momentos fugaces pero decisivos.",
-        director: "Lucia Ravanelli",
-        año: 2016,
-        genero: "Ficción",
-        duración: "Cortometraje",
-        imagen: "assets/img/31.webp",
-    },
-    {
-        titulo: "Les miserables",
-        imagen: "",
-        sinopsis: "En el barrio de la novela de Victor Hugo, devenido al día de hoy en barrio marginal, un policía recién transferido aprende prácticas policiales abusivas de sus dos compañeros más veteranos. La comunidad amenaza con explotar cuando un adolescente logra filmarlos.",
-        director: "Ladj Ly",
-        año: 2017,
-        genero: "Ficción",
-        duración: "Cortometraje",
-        imagen: "assets/img/35.webp",
+        username: "usuario4",
+        password: "contraseña4"
     },
 ];
 
@@ -89,15 +23,21 @@ function iniciarSesion(username, password) {
 
     // Verificar si se encontró un usuario y si la contraseña coincide
     if (usuarioEncontrado && usuarioEncontrado.password === password) {
+        // Almacenar el estado de inicio de sesión en localStorage
+        localStorage.setItem('usuarioLogueado', JSON.stringify(usuarioEncontrado));
         return true; // Acceso correcto
     } else {
         return false; // Acceso denegado
     }
 }
 
-const loginForm = document.getElementById("loginForm");
-const mensajeLogin = document.getElementById("mensajeLogin");
+// Función para obtener el usuario almacenado en localStorage
+function obtenerUsuarioLogueado() {
+    const usuarioGuardado = localStorage.getItem('usuarioLogueado');
+    return usuarioGuardado ? JSON.parse(usuarioGuardado) : null;
+}
 
+// Modificar el evento submit del formulario
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault(); // Evitar que el formulario se envíe
 
@@ -108,53 +48,84 @@ loginForm.addEventListener("submit", function (event) {
     // Verificar usuario
     if (iniciarSesion(username, password)) {
         mensajeLogin.textContent = "Bienvenido a Contrapikado";
-        // Redirigir al usuario a la página principal
+
+        location.reload();
     } else {
         mensajeLogin.textContent = "Inténtalo de nuevo.";
     }
 });
 
-function mostrarPeliculasEnPagina() {
+// Verificar el estado de inicio de sesión al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+    const usuarioLogueado = obtenerUsuarioLogueado();
+
+    if (usuarioLogueado) {
+        mensajeLogin.textContent = "Bienvenido nuevamente, " + usuarioLogueado.username + "!";
+        //swal("Bienvenido nuevamente", usuarioLogueado.username + "!");
+
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Bienvenido a Contrapikado",
+            showConfirmButton: false,
+            timer: 1500
+          });
+
+    }
+});
+
+
+// Modificar la función mostrarPeliculasEnPagina para cargar películas de manera asíncrona
+async function mostrarPeliculasEnPagina() {
     const peliculasContainer = document.getElementById("peliculasContainer");
 
-    peliculas.forEach(pelicula => {
-        const peliculaDiv = document.createElement("div");
-        peliculaDiv.className = "pelicula";
+    try {
+        // Utilizar fetch para obtener las películas de un archivo JSON
+        const response = await fetch('movies.json');
+        const peliculas = await response.json();
 
-        const titulo = document.createElement("h2");
-        titulo.textContent = pelicula.titulo;
+        // Mostrar las películas en la página
+        peliculas.forEach(pelicula => {
+            const peliculaDiv = document.createElement("div");
+            peliculaDiv.className = "pelicula";
 
-        const director = document.createElement("p");
-        director.textContent = "Director: " + pelicula.director;
+            const titulo = document.createElement("h2");
+            titulo.textContent = pelicula.titulo;
 
-        const genero = document.createElement("p");
-        genero.textContent = "Género: " + pelicula.genero;
+            const director = document.createElement("p");
+            director.textContent = "Director: " + pelicula.director;
 
-        const imagen = document.createElement("img");
-        imagen.src = pelicula.imagen;
-        imagen.alt = pelicula.titulo;
+            const genero = document.createElement("p");
+            genero.textContent = "Género: " + pelicula.genero;
 
-        const verMasTardeButton = document.createElement("button");
-        verMasTardeButton.textContent = "Ver más tarde";
-        verMasTardeButton.addEventListener("click", function () {
-            agregarAVerMasTarde(pelicula);
+            const imagen = document.createElement("img");
+            imagen.src = pelicula.imagen;
+            imagen.alt = pelicula.titulo;
+
+            const verMasTardeButton = document.createElement("button");
+            verMasTardeButton.textContent = "Ver más tarde";
+            verMasTardeButton.addEventListener("click", function () {
+                agregarAVerMasTarde(pelicula);
+            });
+
+            const favoritosButton = document.createElement("button");
+            favoritosButton.textContent = "Favoritos";
+            favoritosButton.addEventListener("click", function () {
+                agregarAFavoritos(pelicula);
+            });
+
+            peliculaDiv.appendChild(titulo);
+            peliculaDiv.appendChild(director);
+            peliculaDiv.appendChild(genero);
+            peliculaDiv.appendChild(imagen);
+            peliculaDiv.appendChild(verMasTardeButton);
+            peliculaDiv.appendChild(favoritosButton);
+
+            peliculasContainer.appendChild(peliculaDiv);
         });
-
-        const favoritosButton = document.createElement("button");
-        favoritosButton.textContent = "Favoritos";
-        favoritosButton.addEventListener("click", function () {
-            agregarAFavoritos(pelicula);
-        });
-
-        peliculaDiv.appendChild(titulo);
-        peliculaDiv.appendChild(director);
-        peliculaDiv.appendChild(genero);
-        peliculaDiv.appendChild(imagen);
-        peliculaDiv.appendChild(verMasTardeButton);
-        peliculaDiv.appendChild(favoritosButton);
-
-        peliculasContainer.appendChild(peliculaDiv);
-    });
+    } catch (error) {
+        console.error('Error al cargar las películas:', error);
+    }
 }
 
 // Llama a la función para mostrar películas en la página
@@ -163,10 +134,12 @@ mostrarPeliculasEnPagina();
 const verMasTardeList = [];
 const favoritosList = [];
 
+
 function agregarAVerMasTarde(pelicula) {
     if (!verMasTardeList.includes(pelicula)) {
         verMasTardeList.push(pelicula);
         mostrarPeliculasEnVerMasTarde();
+        guardarEnLocalStorage('verMasTardeList', verMasTardeList);
     }
 }
 
@@ -175,6 +148,7 @@ function quitarDeVerMasTarde(pelicula) {
     if (index !== -1) {
         verMasTardeList.splice(index, 1);
         mostrarPeliculasEnVerMasTarde();
+        guardarEnLocalStorage('verMasTardeList', verMasTardeList);
     }
 }
 
@@ -182,6 +156,7 @@ function agregarAFavoritos(pelicula) {
     if (!favoritosList.includes(pelicula)) {
         favoritosList.push(pelicula);
         mostrarPeliculasEnFavoritos();
+        guardarEnLocalStorage('favoritosList', favoritosList);
     }
 }
 
@@ -190,8 +165,15 @@ function quitarDeFavoritos(pelicula) {
     if (index !== -1) {
         favoritosList.splice(index, 1);
         mostrarPeliculasEnFavoritos();
+        guardarEnLocalStorage('favoritosList', favoritosList);
     }
 }
+
+// Función para guardar en localStorage
+function guardarEnLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
 
 function mostrarPeliculasEnVerMasTarde() {
     const verMasTardeContainer = document.getElementById("watchLaterList");
@@ -230,3 +212,23 @@ function mostrarPeliculasEnFavoritos() {
         favoritosContainer.appendChild(peliculaItem);
     });
 }
+
+
+//Empleo de funciones de orden superior para filtro.
+
+function filtrarPeliculas(peliculas, filtro) {
+    return peliculas.filter(filtro);
+}
+
+// Definir funciones de filtro
+function esDocumental(pelicula) {
+    return pelicula.genero === "Documental";
+}
+
+function esFiccion(pelicula) {
+    return pelicula.genero === "Ficción";
+}
+
+// Usar la función de orden superior para filtrar películas
+const documentales = filtrarPeliculas(peliculas, esDocumental);
+const peliculasDeFiccion = filtrarPeliculas(peliculas, esFiccion);
